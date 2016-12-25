0a477f67d87329bfd5528c135503a9487f4c8bf5
Milo Hartsoe <milo.red.duck@gmail.com>
2016-12-25 19:14:01 +0000

    Add cl access from js and file storage

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3500;
+var uploadPath = path.join(__dirname, 'uploads');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, uploadPath);
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now());
+    }
+})
+
+var upload = multer({
+    storage: storage
+});
 
 //home
 app.get('/', function(req, res) {
@@ -15,13 +29,24 @@ app.get('/', function(req, res) {
 });
 
 //POST image for metadata removal
-app.post('/upload', function(req, res) {
+app.post('/upload', upload.single('file'), function(req, res) {
+    var file = req.file;
+    if (file) {
+        console.log(file.path);
+        var exec = require('child_process').exec;
+        var cmd = ' ';// + uploadPath;
+        exec(cmd, function(error, stdout, stderr) {
+            if(error) throw error;
+            console.log(stdout);
+        });
 
-    /*var exec = require('child_process').exec;
-    var cmd = ' ' + ;
-
-    exec(cmd, function(error, stdout, stderr) {});
-    */
+    }
+    else {
+        res.json({
+            error: 'File not readable'
+        });
+    }
+    res.end('end');
 });
 
 app.listen(PORT, function() {
